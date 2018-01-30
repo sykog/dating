b4ace7a5fa84b3e87015f8c538e3c6c6797ecc3b
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-30 00:09:32 -0500

    form 1 validates

--- index.php
+++ index.php
@@ -18,6 +18,30 @@
 
     //Define a form 1(order) route
     $f3->route('GET|POST /personal', function($f3, $params) {
+            if(isset($_POST['submit'])){
+                $first = $_POST['first'];
+                $last = $_POST['last'];
+                $age = $_POST['age'];
+                $gender = $_POST['gender'];
+                $phone = $_POST['phone'];
+
+                $_SESSION['first'] = $first;
+                $_SESSION['last'] = $last;
+                $_SESSION['age'] = $age;
+                $_SESSION['gender'] = $gender;
+                $_SESSION['phone'] = $phone;
+
+                include('model/validate.php');
+
+                $f3->set('first', $first);
+                $f3->set('last', $last);
+                $f3->set('age', $age);
+                $f3->set('gender', $gender);
+                $f3->set('phone', $phone);
+                $f3->set('errors', $errors);
+                $f3->set('success', $success);
+            }
+
             $template = new Template();
             echo $template->render('pages/form1.html');
         }
---
+++ model/validate.php
@@ -0,0 +1,42 @@
+<?php
+    // create empty array of errors
+    $errors = array();
+
+    if(!validName($first)){
+        $errors['first'] = "Please enter a valid name.";
+    }
+    if(!validName($last)){
+        $errors['last'] = "Please enter a valid name.";
+    }
+    if(!validAge($age)){
+        $errors['age'] = "Must be 18 or older";
+    }
+    if(!validPhone($phone)){
+        $errors['phone'] = "Invalid. Numbers and dashes only.";
+    }
+
+    // must be true to submit
+    $success = sizeof($errors) == 0;
+
+    // name must be alphabetic
+    function validName($name){
+        return ctype_alpha($name);
+    }
+
+    // must be at least 18
+    function validAge($age){
+        return $age >= 18;
+    }
+
+    // only numbers and dashes (10 digits)
+    function validPhone($phone){
+        if (is_numeric($phone) || (strpos($phone, "-") && !ctype_alpha($phone))){
+            if (strpos($phone, "-") && (strlen($phone) == 12)){
+                return true;
+            }
+            if (strlen($phone) == 10){
+                return true;
+            }
+        }
+        return false;
+    }
