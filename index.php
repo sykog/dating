1c7e5c3ab894af75b3ab4249a88f6fb02e9892c3
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-29 20:09:49 -0500

    create form links to new page

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
+    //Start the session
+    session_start();
     // require autoload file
     require_once('vendor/autoload.php');
 
     // create instance of base class
     $f3 = Base::instance();
+    // set debug level
+    $f3->set('DEBUG', 3);
 
-    // define a route using a template
+    // define a default route using a template
     $f3->route('GET /', function() {
             $template = new Template();
             echo $template->render('pages/home.html');
+        }
+    );
+
+    //Define a form 1(order) route
+    $f3->route('GET /personal', function($f3, $params) {
+            $template = new Template();
+            echo $template->render('pages/form1.html');
         }
     );
 
