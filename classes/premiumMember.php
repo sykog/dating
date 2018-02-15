cc67a635d8a86e4e978c8b2160bc09bb475821bb
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-02-15 18:29:33 -0500

    getters and setters for premiumMemeber, created premium checkbox

--- classes/premiumMember.php
+++ classes/premiumMember.php
@@ -1,6 +1,45 @@
 <?php
 
-    class PremiumMember
+    class PremiumMember extends Member
     {
+        private $_indoorInterests = array();
+        private $_outdoorInterests = array();
 
+        /**
+         * get the array of indoor interests
+         * @return array indoor interests
+         */
+        function getIndoorInterests()
+        {
+            return $this->_indoorInterests;
+        }
+
+        /**
+         * set the indoor interests selected
+         * @param indoorInterests indoor interests
+         * @return void
+         */
+        function setIndoorInterests($indoorInterests)
+        {
+            $this->_indoorInterests = $indoorInterests;
+        }
+
+        /**
+         * get the array of outdoor interests
+         * @return array outdoor interests
+         */
+        function getOutdoorInterests()
+        {
+            return $this->_outdoorInterests;
+        }
+
+        /**
+         * set the outdoor interests selected
+         * @param outdoorInterests indoor interests
+         * @return void
+         */
+        function setOutdoorInterests($outdoorInterests)
+        {
+            $this->_outdoorInterests = $outdoorInterests;
+        }
     }
