c78fd03ed7e0b9697c739ec4c33f19c9c5a8fa51
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-02-28 00:50:08 -0500

    created table and linked to it

---
+++ model/database.php
@@ -0,0 +1,18 @@
+<?php
+/* CREATE TABLE Members (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
+    fname VARCHAR(30),
+    lname VARCHAR(30),
+    age TINYINT,
+    gender VARCHAR(6),
+    phone CHAR(9),
+    email VARCHAR(60),
+    state VARCHAR(20),
+    seeking VARCHAR(6),
+    bio TEXT,
+    premium TINYINT,
+    image VARCHAR(100),
+    interests VARCHAR(150)
+) */
+
+require_once("/home/asuarezg/config.php");
