69e907caf597e19ca798b4ef530f8da1be7f57f4
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-02-28 01:16:03 -0500

    created Database class, constructor, defined insert query

--- model/database.php
+++ model/database.php
@@ -16,3 +16,41 @@
 ) */
 
 require_once("/home/asuarezg/config.php");
+
+/**
+ * The Member class represents a member on a dating website.
+ * Members have first and last names, an age, gender, phone number,
+ * email, home state, bio, and a gender they are seeking
+ * @author Antonio Suarez <asuarez2@mail.greenriver.edu>
+ * @copyright 2018
+ */
+class Database
+{
+    protected $dbh;
+
+    /**
+     * Database constructor.
+     * Establishes connection with database
+     * @return void
+     */
+    function __construct()
+    {
+        try {
+            // instantiate a PDO object
+            $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    function addMember()
+    {
+        // define the query
+        $sql = "INSERT INTO Members(fname, lname, age, gender, phone, email, state, seeking, bio, premium, image, interests)
+          VALUES (:fname, :lname, :age, :gender, :phone, :email, :state, :seeking, :bio, :premium, :image, :interests)";
+
+        // prepare the statement
+        $statement = $dbh->prepare($sql);
+    }
+}
